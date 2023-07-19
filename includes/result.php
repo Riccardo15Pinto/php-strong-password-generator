459cce3c8d4f6358ed7c78fe1d4ca7df1e9ec30f
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-19 17:48:03 +0200

    include head.php

--- includes/result.php
+++ includes/result.php
@@ -7,11 +7,8 @@ $new_password = $_SESSION['password'];
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php include __DIR__ . '/head.php'  ?>
+
 
 <body>
     <div>
