3a9200a9468bd800c4706a8df716bd0241cd14c3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-19 15:10:35 +0200

    add input

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 session_start();
 include __DIR__ . '/includes/functions.php';
 $password = $_GET['password'] ?? null;
-if (isset($password)) {
+if (isset($password) && $password) {
     header("location:includes/result.php");
     $new_password = get_random_Password(intval($password));
     $_SESSION['password'] = $new_password;
 }
+
+$radio = $_GET['radio'] ?? '';
+$font = $_GET['font'] ?? '';
+$number = $_GET['number'] ?? '';
+$symbol = $_GET['symbol'] ?? '';
+var_dump($radio, $font, $number, $symbol);
 
 ?>
 
@@ -24,7 +30,20 @@ if (isset($password)) {
         <h1>Crea la tua Password</h1>
         <form action="">
             <input type="number" name="password" value="<?= $password ?>">
+            <label for="yes">SI</label>
+            <input type="radio" name="radio" id="yes" value="si">
+            <label for="no">NO</label>
+            <input type="radio" name="radio" id="no" value="no">
+            <label for="font">LETTERE</label>
+            <input type="checkbox" name="font" id="font">
+            <label for="number">NUMERI</label>
+
+            <input type="checkbox" name="number" id="number">
+            <label for="symbol">SIMBOLI</label>
+
+            <input type="checkbox" name="symbol" id="symbol">
             <button type="submit">Invia</button>
+            <button type="reset">Reset</button>
         </form>
 
     </main>
