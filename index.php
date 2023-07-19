0f784d1122c5d1f6e571e3edbce30f4105ebd5a8
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-19 18:24:07 +0200

    change checkbox in radiobox

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $number = $_GET['number'] ?? '';
 
 $symbol = $_GET['symbol'] ?? '';
 
-if (isset($password) && $password) {
+if (isset($password) && $password && $password > 0) {
     header("location:includes/result.php");
     $new_password = get_random_Password($password, $radio, $font, $number, $symbol);
     $_SESSION['password'] = $new_password;
@@ -28,7 +28,7 @@ if (isset($password) && $password) {
 <?php include __DIR__ . '/includes/head.php'  ?>
 
 <body>
-    <main>
+    <main class="container m-auto p-5">
         <h1 class="text-primary">Crea la tua Password</h1>
         <form action="">
             <input type="number" name="password" value="<?= $password ?>">
@@ -41,22 +41,28 @@ if (isset($password) && $password) {
             </div>
             <div>
                 <h3>Desideri non avere lettere maiuscole?</h3>
-                <label for="font">LETTERE</label>
-                <input type="checkbox" name="font" id="font">
+                <label for="yes">SI</label>
+                <input type="radio" name="font" id="yes" value="si">
+                <label for="no">NO</label>
+                <input type="radio" name="font" id="no" value="">
             </div>
             <div>
                 <h3>Desideri non avere numeri?</h3>
-                <label for="number">NUMERI</label>
-                <input type="checkbox" name="number" id="number">
+                <label for="yes">SI</label>
+                <input type="radio" name="number" id="yes" value="si">
+                <label for="no">NO</label>
+                <input type="radio" name="number" id="no" value="">
             </div>
             <div>
                 <h3>Desideri non avere caratteri speciali?</h3>
-                <label for="symbol">SIMBOLI</label>
-                <input type="checkbox" name="symbol" id="symbol">
+                <label for="yes">SI</label>
+                <input type="radio" name="symbol" id="yes" value="si">
+                <label for="no">NO</label>
+                <input type="radio" name="symbol" id="no" value="">
             </div>
             <div>
-                <button type="submit">Invia</button>
-                <button type="reset">Reset</button>
+                <button type="submit" class="btn btn-primary m-2">Invia</button>
+                <button type="reset" class="btn btn-warning m-2">Reset</button>
             </div>
         </form>
 
