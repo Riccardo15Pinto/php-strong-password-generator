15fb06dcaa1a7fe84f1bff86aa0a0b082c6eb25c
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-19 14:41:46 +0200

    add session start , header , $session

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+session_start();
 include __DIR__ . '/includes/functions.php';
 $password = $_GET['password'] ?? null;
 if (isset($password)) {
-
+    header("location:includes/result.php");
     $new_password = get_random_Password(intval($password));
+    $_SESSION['password'] = $new_password;
 }
-
 
 ?>
 
