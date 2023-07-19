c586630a27a721583dab5defc58abe3bee757119
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-19 17:32:05 +0200

    set function get_random_password

--- includes/functions.php
+++ includes/functions.php
@@ -1,38 +1,30 @@
 <?php
-//$first_filter, $second_filter, $third_filter, $fourth_filter
-function get_random_Password($length_password, $first_filter)
+function get_random_Password($length_password, $no_repeat_filter, $big_font_filter, $number_filter, $symbol_filter)
 {
-    $alphabet_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-!£$%&?@#';
+    $small_font = 'abcdefghijklmnopqrstuvwxyz';
+    $big_font = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $number = '1234567890';
+    $symbol = '-!£$%&?@#';
+    $alphabet_characters = $small_font . ' ' . $big_font . ' ' . $number . ' ' . $symbol;
+    $password = [];
+    $alphabet_Length = strlen($alphabet_characters) - 1;
 
-    if ($first_filter) {
-        $password = []; //remember to declare $pass as an array
-        $alphabet_Length = strlen($alphabet_characters) - 1; //put the length -1 in cache
-        // while (count($password) < $length_password) {
-        // }
+    if ($big_font_filter) $alphabet_characters = $small_font . ' ' . $number . ' ' . $symbol;
+    if ($number_filter) $alphabet_characters = $small_font . ' ' . $big_font . ' ' . $symbol;
+    if ($symbol_filter) $alphabet_characters = $small_font . ' ' . $number . ' ' . $big_font;
+    if ($big_font_filter && $symbol_filter && $number_filter) $alphabet_characters = $small_font;
 
-        do {
-            $single_character = rand(0, $alphabet_Length);
-
-            if (!in_array($single_character, $password)) {
-                $password[] = $alphabet_characters[$single_character];
+    do {
+        $alphabet_index = rand(0, $alphabet_Length);
+        $char = $alphabet_characters[$alphabet_index];
+        if ($no_repeat_filter && $length_password < 20) {
+            if (!in_array($char, $password)) {
+                $password[] = $char;
             }
-        } while (count($password) < $length_password);
-        return implode($password); //turn the array into a string
-    }
+        } else {
+            $password[] = $char;
+        }
+    } while (count($password) < $length_password);
 
-
-
-    $password = []; //remember to declare $pass as an array
-    $alphabet_Length = strlen($alphabet_characters) - 1; //put the length -1 in cache
-    for ($i = 0; $i < $length_password; $i++) {
-        $single_character = rand(0, $alphabet_Length);
-        $password[] = $alphabet_characters[$single_character];
-    }
-    return implode($password); //turn the array into a string
-
+    return implode($password);
 }
-    
-    
-    // $taboo = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    // $new_alphabet_characters = str_replace($taboo, '', $alphabet_characters);
-    // return $new_alphabet_characters;
